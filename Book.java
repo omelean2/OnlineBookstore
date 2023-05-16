4a9f5066971971bc3cb9d126c52e967a5e008680
Oswal Melean <56838320+omelean2@users.noreply.github.com>
2023-05-16 10:40:39 -0400

    Create Book.java

---
+++ Book.java
@@ -0,0 +1,46 @@
+public class Book {
+    private String title;
+    private String author;
+    private String genre;
+    private double price;
+
+
+    public Book(String title, String author, String genre, double price) {
+        this.title = title;
+        this.author = author;
+        this.genre = genre;
+        this.price = price;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
